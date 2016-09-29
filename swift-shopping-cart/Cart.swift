e10ec6e537105bfec215af0f2b9035adda9353c6
Victoria Melendez <Victoria@Victorias-MacBook-Pro.local>
2016-09-29 14:02:32 -0400

    done

--- swift-shopping-cart/Cart.swift
+++ swift-shopping-cart/Cart.swift
@@ -7,6 +7,69 @@
 //
 
 class Cart {
+    var items: [Item] = []
     
     
-}
+//    init(items:[Item]){
+//        self.items = items}
+    
+    func totalPriceInCents()-> Int{
+        var totalPriceInArr = 0
+        //total cost of the items in the array
+        for num in items{
+            totalPriceInArr += num.priceInCents
+        }
+        return totalPriceInArr
+    }
+    
+    func add(item:Item) {
+        items.append(item)
+    }
+    
+    func remove(item:Item) {
+        items = items.filter{$0 != item}
+    }
+    
+    func items(withName name: String)-> [Item]{
+        var itemsArr = [Item]()
+        for item in items{
+            if item.name == name {
+                itemsArr.append(item)
+            }
+            
+        }
+        return itemsArr
+        
+    }
+    
+    func items(withMinPrice price:Int) ->[Item]{
+        var itemsNum: [Item] = []
+        for item in items {
+            if price <= item.priceInCents{
+                itemsNum.append(item)
+            }
+        }
+        return itemsNum
+        }
+    
+    
+    func items(withMaxPrice price:Int) ->[Item]{
+        var itemsMax: [Item] = []
+        for item in items {
+            if price >= item.priceInCents {
+                itemsMax.append(item)
+            }
+        }
+        return itemsMax
+        
+        
+    }
+    
+
+    }
+    
+    
+
+
+
+
